3eb9f54406c43d9b5d31252cf0a9e3eba8a3a9e8
Nur Muhammad <blog.nurmuhammad@gmail.com>
2020-11-11 06:18:29 +0800

    Fix port for heroku

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 const { phoneNumberFormatter } = require('./helpers/formatter');
 const fileUpload = require('express-fileupload');
 const axios = require('axios');
+const port = process.env.PORT || 8000;
 
 const app = express();
 const server = http.createServer(app);
@@ -239,6 +240,6 @@ app.post('/send-group-message', [
   });
 });
 
-server.listen(8000, function() {
-  console.log('App running on *: ' + 8000);
+server.listen(port, function() {
+  console.log('App running on *: ' + port);
 });
