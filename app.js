dd4e1bbe9bcffcffffe00696be5535838b784279
Nur Muhammad <blog.nurmuhammad@gmail.com>
2021-03-19 10:04:17 +0800

    Add audio reply

--- app.js
+++ app.js
@@ -64,6 +64,9 @@ client.on('message', msg => {
       sendMediaAsSticker: true, 
       quotedMessageId: msg.id._serialized
     });
+  } else if (msg.body == '!audio') {
+    const media = MessageMedia.fromFilePath('./basmallah.mp3');
+    msg.reply(media);
   } else if (msg.body == '!groups') {
     client.getChats().then(chats => {
       const groups = chats.filter(chat => chat.isGroup);
